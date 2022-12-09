13c19a3a3d7f3496fb5f8edbb1faf89cb6ca9392
Ana <acfiallo@mit.edu>
2022-12-09 00:28:30 -0500

    fixing again the results folder path string to avoid syntax error

--- code/perfect_preview/run_experiments.py
+++ code/perfect_preview/run_experiments.py
@@ -29,7 +29,7 @@ if __name__ == "__main__":
 
     # turbine_rotation_speed = np.sin(0.6) # math.inf
     control_type = "standard"
-    results_folder_path = os.path.join(f"./experiments/pp_WHOI_lidar_h=104m_Tmin={T_min}_Tmax={T_max}_degThreshold={deg_threshold}_nacelleDegRotSpeed={deg_threshold}/")
+    results_folder_path = f"./experiments/pp_WHOI_lidar_h=104m_Tmin={T_min}_Tmax={T_max}_degThreshold={deg_threshold}_nacelleDegRotSpeed={deg_threshold}/"
     
     if not os.path.exists(results_folder_path):
         logger.info("Making directory path to store experiment results")
